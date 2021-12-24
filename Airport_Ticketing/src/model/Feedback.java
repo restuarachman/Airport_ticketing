ca9f5cbaf96b836388f17bc08bdf64db5ae2f987
irwanrezaf <irwanrezaf10@gmail.com>
2021-12-24 22:12:30 +0700

    Tambah Feedback Class

---
+++ Airport_Ticketing/src/model/Feedback.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Feedback {
+    private int id;
+    private String ulasan;
+    private double rating;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUlasan() {
+        return ulasan;
+    }
+
+    public void setUlasan(String ulasan) {
+        this.ulasan = ulasan;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+    
+    public void getFeedback(){
+        
+    }
+    public void setFeedback(){
+        
+    }
+    public void showFeedback(){
+        
+    }
+}
